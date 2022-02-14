47e9fe3fe89b417a7e6515e59b48f3b2b5d171e9
gaddin <80589591+gaddin@users.noreply.github.com>
2022-02-14 19:49:28 +0100

    Update kernel.c

--- kernel/boot/kernel.c
+++ kernel/boot/kernel.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 void _start()
 {
+	//this is a test string
 	__vga__putchar__('>', 1, 7, VGA_LIGHT_CYAN);
 	__vga__putchar__('>', 2, 7, VGA_LIGHT_RED);
 	asm volatile("hlt\n\t");
