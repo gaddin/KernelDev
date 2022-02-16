44972d76f61e63eca6296d294164ec7d00c52759
gaddin <80589591+gaddin@users.noreply.github.com>
2022-02-16 21:25:04 +0100

    Delete kernel directory

--- kernel/boot/strings.asm
+++
@@ -1,9 +0,0 @@
-OSname:
-	db				"\[CCROSS-Bootloader version 0.01\[r\n\n", 0x0
-str:
-    db              "protected mode:             [\[COK!\[r]\n", 0x0
-sector2:
-	db				"sector \[C2\[r reached!           [\[COK!\[r]\n", 0x0
-err:
-	db				"\n\n\[RDISK ERR!-> \[Csector2\[R SKIPPED!!\[r\n", 0x0
-
