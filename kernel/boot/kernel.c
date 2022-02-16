2c9e78e148f421208ac8945c425225c4acbe3311
gaddin <80589591+gaddin@users.noreply.github.com>
2022-02-16 21:24:22 +0100

    Delete kernel directory

--- kernel/boot/kernel.c
+++
@@ -1,9 +0,0 @@
-#include "../../include/crstd.h"
-void _start()
-{
-	//this is a test string
-	__vga__putchar__('>', 1, 7, VGA_LIGHT_CYAN);
-	__vga__putchar__('>', 2, 7, VGA_LIGHT_RED);
-	asm volatile("hlt\n\t");
-}
-
--- kernel/boot/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nasm -f bin boot.asm -o boot.bin
-gcc -fno-pic -nostdlib -ffreestanding -c kernel.c -o kernel.o
-ld  -o kernel.bin -Ttext 0x7f00 kernel.o --oformat binary
-cat boot.bin kernel.bin > kernel.elf
-doas dd if=/dev/zero of=/kernel.iso bs=512 count=1440
-dd if=kernel.elf of=kernel.iso  conv=notrunc bs=1024 count=2
-qemu-system-i386 -k 10 kernel.iso -full-screen
