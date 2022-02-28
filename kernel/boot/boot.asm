66efa8ae8b3d2f5c3fbdebda36d3158bfd8dd59f
gaddin <80589591+gaddin@users.noreply.github.com>
2022-02-28 12:23:19 +0100

    Update boot.asm

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -13,10 +13,10 @@ _entry16:
 	xor cx, cx
 	mov dx, 0x184f
 	int 0x10
-	mov ah, 0x2		;
-	mov al, 0x80		; <---TODO: FIX DAT SHIT(maybe)
-	mov ch, 0x1		;
-	mov cl, 0x2		;
+	mov ah, 0x2		
+	mov al, 0x80		
+	mov ch, 0x1		
+	mov cl, 0x2		
 	mov dh, 0x80
 	mov dl, 0x1
 	mov bx, 0x7e00
