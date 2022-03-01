4bf628a41696db378d0cf293a7d33baedd4d347b
gaddin <80589591+gaddin@users.noreply.github.com>
2022-03-01 20:47:32 +0100

    Update kern.sh

--- boot/kern.sh
+++ boot/kern.sh
@@ -1,6 +1,6 @@
-/home/espi/build-i686-elf/linux/output/bin/i686-elf-gcc -ffreestanding -nostdlib -c main.c -o main.o -save-temps=obj
+$HOME/build-i686-elf/linux/output/bin/i686-elf-gcc -ffreestanding -nostdlib -c main.c -o main.o -save-temps=obj
 nasm -f bin boot.asm -o boot.bin
-/home/espi/build-i686-elf/linux/output/bin/i686-elf-ld -o main.bin -Ttext 0x7ef0 main.o --oformat binary
+$HOME/build-i686-elf/linux/output/bin/i686-elf-ld -o main.bin -Ttext 0x7ef0 main.o --oformat binary
 cat boot.bin main.bin > kernel.elf
 qemu-system-i386 kernel.elf 
 
