2e366f31d4826178c7cc3d35bdc118d9c5f1a802
gaddin <80589591+gaddin@users.noreply.github.com>
2022-03-01 21:02:00 +0100

    Update VGA.h

--- boot/VGA.h
+++ boot/VGA.h
@@ -5,13 +5,13 @@
 	VGA COLOR MACROS
 */
 #define VGA_BLACK 0x0
-#define 	VGA_BLUE 0x1  
-#define 	VGA_GREEN 0x2
+#define VGA_BLUE 0x1  
+#define VGA_GREEN 0x2
 #define	VGA_CYAN 0x3
-#define 	VGA_RED 0x4
+#define VGA_RED 0x4
 #define	VGA_MAGENTA 0x5
-#define 	VGA_BROWN 0x6
+#define VGA_BROWN 0x6
 #define	VGA_WHITE 0xf
-#define 	VGA_GRAY 0x8
+#define VGA_GRAY 0x8
 
 #endif
