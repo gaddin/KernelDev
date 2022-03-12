910a8290ee36fc9a8b83079bf4cd3ab743ed9c47
gaddin <80589591+gaddin@users.noreply.github.com>
2022-03-12 10:13:00 +0100

    changed Kernel Position

--- boot/canr.sh
+++ boot/canr.sh
@@ -1,6 +1,6 @@
 $HOME/build-i686-elf/linux/output/bin/i686-elf-gcc -ffreestanding -nostdlib -c main.c -o main.o -save-temps=obj
 nasm -f bin boot.asm -o boot.bin
-$HOME/build-i686-elf/linux/output/bin/i686-elf-ld -o main.bin -Ttext 0x7ef0 main.o --oformat binary
+$HOME/build-i686-elf/linux/output/bin/i686-elf-ld -o main.bin -Ttext 0x7f00 main.o --oformat binary
 cat boot.bin main.bin > kernel.elf
 qemu-system-i386 kernel.elf 
 
