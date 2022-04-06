74103ca686abdcbbfa7451e3b4c71d8363d4a39b
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-06 11:11:20 +0200

    Create hwio.c

---
+++ boot/hwio.c
@@ -0,0 +1,38 @@
+#include "hwio.c"
+
+void outb(size_t port, u8 data)
+{
+  asm volatile (
+   "outb %0, %1\n\t"
+    : "r" (data)
+    : "=r" (port)
+  );
+}
+u8 inb(size_t port)
+{
+  u8 data;
+  asm volatile (
+    "inb %0, %1\n\t"  
+    : "r" (port)
+    : "=r" (data)
+  );
+  return data;
+}
+void outw(size_t port, u16 data)
+{
+  asm volatile (
+    "outw %0, %1\n\t"
+    : "r" (data)
+    : "=r" (port)
+  );
+}
+void inw(size_t port)
+{
+  u16 data;
+  asm volatile (
+    "inw %0, %1\n\t"
+    : "r" (data)
+    : "=r" (port)
+  );
+  return data;
+}
