5677f724a18c21b717b191e8b29939981c5fb17d
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-06 09:29:05 +0200

    Update kernel_entry.c

--- boot/kernel_entry.c
+++ boot/kernel_entry.c
@@ -1,10 +1,11 @@
 #include "VGA.h"
+#include "PCI.h"
 int _start()
 {	
 	/*
 	general setup 
 	*/
-	typedef void VIDEO_MODE;
+	
 	VIDEO_MODE (*video_mode)(void) = NULL;
 	//starting the kernel with the correct videomode
 	k_start(video_mode);
