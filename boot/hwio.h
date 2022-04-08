cac8da579575b65ed91e2dc6a5ca543dc204154e
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-08 19:03:59 +0200

    Update hwio.h

--- boot/hwio.h
+++ boot/hwio.h
@@ -3,10 +3,10 @@
 /*includes*/
 #include "types.h"
 
-void outb(size_t port, u8 data); // 8-bit value
-u8 inb(size_t port);
+void out8(size_t port, u8 data); // 8-bit value
+u8 in8(size_t port);
 
-void outw(size_t port, u16 data); // 16-bit value
-u16 inw(size_t port);
+void out16(size_t port, u16 data); // 16-bit value
+u16 in16(size_t port);
 
 #endif
