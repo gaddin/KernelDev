9f0de2629ce842ce2661c91bfa56a9f7c17944ee
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-13 01:46:46 +0200

    Delete boot directory

--- boot/devices.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEVICES_H
-#define DEVICES_H
-
-#include "VGA.h"
-//#include "PCI.h"
-
-#endif
--- boot/kernel_entry.c
+++
@@ -1,8 +0,0 @@
-#include "VGA.h"
-#include "hwio.h"
-int32_t _start()
-{
-	char string[] = "entered kernel";
-	vga_prints(string,0,1);
-	return 0;
-}
