37be5b2a5232e83ac729e6b43bde3caf939906f2
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-17 00:07:28 +0200

    Update hwio.c

--- boot/hwio.c
+++ boot/hwio.c
@@ -47,16 +47,3 @@ uint16_t in16(uint16_t port)
     
     return data ;
 }
-
-
-void enable_a20() 
-{
-    char success[] = "enabled a20 line!" ;
-    
-    uint8_t a20 = in8(0x92) ;
-    asm volatile ("or $0x2, %0"
-                    :"=r"(a20)) ;
-    out8(0x92, a20) ;
-    kernel_log_write(success) ; 
-    return ;
-}
