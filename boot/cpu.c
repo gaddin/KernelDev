4e4f61fc9379cf4cf943c73579bee509b5df323c
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-17 00:07:06 +0200

    Update cpu.c

--- boot/cpu.c
+++ boot/cpu.c
@@ -7,7 +7,14 @@ void load_idt (idt_ptr *idt_register)
 }
 
 
-void enable_a20()
+void enable_a20() 
 {
-
+    char success[] = "enabled a20 line!" ;
+    
+    uint8_t a20 = in8(0x92) ;
+    asm volatile ("or $0x2, %0"
+                    :"=r"(a20)) ;
+    out8(0x92, a20) ;
+    kernel_log_write(success) ; 
+    return ;
 }
