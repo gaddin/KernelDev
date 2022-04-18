c0da9717dcc493eed7752701bc284dc525513eff
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-18 18:09:08 +0200

    Update hwio.h

--- boot/hwio.h
+++ boot/hwio.h
@@ -3,10 +3,14 @@
 
 /* output an 8-bit value to a port */
 void out8(uint16_t port, uint8_t data) ;
+
 /* get an 8-bit value from a port */
 uint8_t in8(uint16_t port) ;
-/* */
-void out16(uint16_t port, uint16_t data) ;  
+
+/* output an 16-bit value to a port */
+void out16(uint16_t port, uint16_t data) ;
+  
+/* get an 16-bit value from a port */
 uint16_t in16(uint16_t port) ;
 
 #endif /* HWIO_H */
