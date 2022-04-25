d3cf10b5e245626c958de80a25573dcc9c8c4aad
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-25 10:29:00 +0200

    packed the idt_descriptor and added a typedef ptr

--- boot/cpu.h
+++ boot/cpu.h
@@ -1,16 +1,18 @@
 #ifndef CPU_H
 #define CPU_H
 
-typedef struct
+typdef struct
 {
     uint16_t offset_0;
     uint16_t selector;
-    uint8_t zero;
+    uint8_t reserved;
     uint8_t type_attributes;
     uint16_t offset_1;
 }
-idt_entry;
+idt_descriptor __attribute__((packed));
 
+typedef idt_descriptor* idt_ptr;
 void load_idt();
 void enable_a20();
+
 #endif /* CPU_H */
