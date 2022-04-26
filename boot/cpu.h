04b16a2b9d30920e3e7cfafc65bc976bd66c861a
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-26 14:51:28 +0200

    Update cpu.h

--- boot/cpu.h
+++ boot/cpu.h
@@ -3,16 +3,43 @@
 
 typedef struct
 {
-    uint16_t offset_0;
-    uint16_t selector;
-    uint8_t _reserved;
-    uint8_t type_attributes;
-    uint16_t offset_1;
+    uint16_t offset_0 ;
+    uint16_t selector ;
+    uint8_t _reserved ;
+    uint8_t type_attributes ;
+    uint16_t offset_1 ;
 }
-idt_descriptor __attribute__((packed));
+idt_descriptor __attribute__((packed)) ;
 
-typedef idt_descriptor idt_ptr;
-void load_idt();
-void enable_a20();
+typedef idt_descriptor idt_ptr ;
+
+void load_idt() ;
+
+uint32_t read_cr0() ;
+uint32_t _reservedread_cr1() ;
+uint32_t read_cr2() ;
+uint32_t read_cr0() ;
+uint32_t read_cr0() ;
+uint32_t read_cr0() ;
+uint32_t read_cr0() ;
+
+
+inline void enable_a20() { 
+
+}
+
+inline void write_cr0() { 
+
+}
+
+inline void write_cr2() { 
+
+}
+
+inline void write_cr3() { 
+
+}
+
+void write_cr5() ;
 
 #endif /* CPU_H */
