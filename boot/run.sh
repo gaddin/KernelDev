74df1f5dda96be36f1362d70272d628399c12df5
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-26 13:24:23 +0200

    Update run.sh

--- boot/run.sh
+++ boot/run.sh
@@ -5,7 +5,7 @@ $HOME/cross-compiler/bin/i686-elf-gcc -nostdlib -ffreestanding -c kernel_entry.c
 $HOME/cross-compiler/bin/i686-elf-gcc -nostdlib -ffreestanding -S kernel_entry.c -o tmp.s
 $HOME/cross-compiler/bin/i686-elf-gcc -nostdlib -ffreestanding -c VGA.c  -o VGA.o
 $HOME/cross-compiler/bin/i686-elf-gcc -nostdlib -ffreestanding -c hwio.c  -o hwio.o
-$HOME/cross-compiler/bin/i686-elf-ld  -o kernel.bin -Ttext 0x7f00 kernel_entry.o VGA.o hwio.o --oformat binary
+$HOME/cross-compiler/bin/i686-elf-ld  -o kernel.bin -Ttext 0x10000 kernel_entry.o VGA.o hwio.o --oformat binary
 cat boot.bin kernel.bin > kernel.elf
 sudo dd if=/dev/zero of=kernel.iso bs=512 count=1440
 dd if=kernel.elf of=kernel.iso  conv=notrunc bs=1024 count=2
