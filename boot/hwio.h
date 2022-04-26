b707e3ab313037b74a7a687d4c8d094b8a524fd7
gaddin <80589591+gaddin@users.noreply.github.com>
2022-04-26 13:34:05 +0200

    Update hwio.h

--- boot/hwio.h
+++ boot/hwio.h
@@ -1,16 +1,43 @@
 #ifndef HWIO_H
 #define HWIO_H
 
-/* output an 8-bit "byte" to a port */
-void outb(uint16_t port, uint8_t data) ;
+inline void outb(uint16_t port, uint8_t data) {
 
-/* get an 8-bit "byte" from a port */
-uint8_t inb(uint16_t port) ;
+    asm volatile ("outb %0, %1" :: "a" (data),"Nd" (port)) ;
+    
+    return;
+}
 
-/* output a 16-bit "word" to a port */
-void outw(uint16_t port, uint16_t data) ;
+
+inline uint8_t inb(uint16_t port) {
+	
+    uint8_t data ;
+    
+    asm volatile ("inb %1, %0" 
+		    	: "=a" (data)
+		    	: "Nd" (port)) ;
+    
+    return data ;
+}
+
+
+inline void outw(uint16_t port, uint16_t data) {
+
+    asm volatile ("outw %0, %1" :: "a" (data), "Nd" (port)) ;
   
-/* get a 16-bit "word" from a port */
-uint16_t inw(uint16_t port) ;
+    return;
+}
+
+
+inline uint16_t inw(uint16_t port) {
+
+    uint16_t data;
+    
+    asm volatile ("inw %1, %0"
+                    : "=a" (data)
+		    		:"Nd" (port)) ;
+    
+    return data ;
+}
 
 #endif /* HWIO_H */
