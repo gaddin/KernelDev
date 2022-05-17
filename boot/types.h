76df81ba3a3c7e0c66892cdf8d9d3ff646adb15b
gaddin <80589591+gaddin@users.noreply.github.com>
2022-05-17 10:56:45 +0200

    Create types.h

---
+++ boot/types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define uint8_t unsigned char
+#define uint16_t unsigned short
+#define uint32_t unsigned int
+#define uint64_t unsigned long
+
+#define size_t uint32_t
+
+#endif /* TYPES_H */
