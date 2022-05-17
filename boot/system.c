94aeafc5bf1fb239451362310c1ce3d42f657342
gaddin <80589591+gaddin@users.noreply.github.com>
2022-05-17 14:58:55 +0200

    Create system.c

---
+++ boot/system.c
@@ -0,0 +1,32 @@
+#include "system.h"
+#include "types.h"
+
+void *memset (void *dst, uint8_t byte, size_t count) {
+
+    for(size_t i = 0; i <= count; i++) {
+        dst[i++] = byte;
+    }
+    
+    return dst;
+}
+
+void memcpy(void *dst, void *src, size_t num) {
+
+    for(size_t i = 0; i <= num; i++) {
+        dst[i++] = src[i++];
+    }
+    return;
+}
+
+void *malloc(size_t size) {
+    
+    
+    return block;
+}
+
+void free(void *ptr) {
+
+    
+    
+    return;
+}
