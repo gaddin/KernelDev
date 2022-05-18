280e41119e053d5faf955f8ce70e36499b2caec6
gaddin <80589591+gaddin@users.noreply.github.com>
2022-05-18 12:52:44 +0200

    Update kernel_entry.c

--- boot/kernel_entry.c
+++ boot/kernel_entry.c
@@ -1,10 +1,13 @@
 #include "graphics.h"
 #include "cpu.h"
-
+#include "ISR.h"
 int32_t _kmain () {
-
+	
     enable_a20() ;
-   
-    
+   	
+	IDT_DESCRIPTOR_32 IDT[IDT_MAX_SIZE];
+	
+	
+	
 	return 0 ;
 }
