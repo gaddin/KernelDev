87232ec4ef09c954b5ca8b52875a4b8acfb6d815
gaddin <80589591+gaddin@users.noreply.github.com>
2022-05-18 08:29:31 +0200

    Update cpu.h

--- boot/cpu.h
+++ boot/cpu.h
@@ -1,31 +1,10 @@
 #ifndef CPU_H
 #define CPU_H
+/*ONLY INCLUDE THIS HEADER NOT IDT_H*/
 
-uint32_t read_cr0() ;
-uint32_t _reservedread_cr1() ;
-uint32_t read_cr2() ;
-uint32_t read_cr0() ;
-uint32_t read_cr0() ;
-uint32_t read_cr0() ;
-uint32_t read_cr0() ;
+#include "types.h"
+#include "IDT.h"
 
-
-inline void enable_a20() {
-
-}
-
-inline void write_cr0() {
-
-}
-
-inline void write_cr2() {
-
-}
-
-inline void write_cr3() {
-
-}
-
-void write_cr5() ;
+void enable_a20();
 
 #endif /* CPU_H */
