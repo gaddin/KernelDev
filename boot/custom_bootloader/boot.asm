44e3d2ad9c3f70875dc9596e0a5a70fa0a468772
gaddin <80589591+gaddin@users.noreply.github.com>
2022-05-24 12:16:52 +0200

    Update boot.asm

--- boot/custom_bootloader/boot.asm
+++ boot/custom_bootloader/boot.asm
@@ -38,20 +38,20 @@ ENTRY:
 .gdtstart:
 .nulldescriptor:
 	dq 0
-.code_descriptor:	    	; cs should point to this descriptor
-	dw 0xffff		; segment limit first 0-15 bits
-	dw 0			; base first 0-15 bits
-	db 0			; base 16-23 bits
-	db 0x9a			; access byte
-	db 0xcf			; high 4 bits (flags) low 4 bits (limit 4 last bits)(limit is 20 bit wide)
-	db 0			; base 24-31 bits
-.data_descriptor:      		; ds, ss, es, fs, and gs should point to this descriptor
-	dw 0xffff		; segment limit first 0-15 bits
-	dw 0			; base first 0-15 bits
-	db 0			; base 16-23 bits
-	db 0x92			; access byte
-	db 0xcf			; high 4 bits (flags) low 4 bits (limit 4 last bits)(limit is 20 bit wide)
-	db 0			; base 24-31 bits
+.code_descriptor:	    	
+	dw 0xffff		
+	dw 0			
+	db 0			
+	db 0x9a			
+	db 0xcf			
+	db 0			
+.data_descriptor:      		
+	dw 0xffff		
+	dw 0		
+	db 0		
+	db 0x92		
+	db 0xcf			
+	db 0			
 .gdtend:
 .gdtdes:
 	dw .gdtend - .gdtstart - 1
